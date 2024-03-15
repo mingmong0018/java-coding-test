4743f05236517f3b491e4fa228294aaa8f272959
김민정 <mingmong0018@gmail.com>
2024-03-15 22:51:14 +0900

    feat: LinkedList 구현

--- src/Structure/LinkedList.java
+++ src/Structure/LinkedList.java
@@ -1,89 +1,66 @@
 package Structure;
+class Node<T> {
+	T data;
+	Node<T> next;
+	public Node(T data) {
+		this.data = data;
+	}
+}
+public class LinkedList<T> {
+	private Node<T> head;
+	public void add (T data) {
+		Node<T> node = new Node<>(data);
+		if (head == null) {
+			head = node;
+			return;
+		}
+		Node<T> current = head;
+		while(current.next!=null) {
+			current = current.next;
+		}
+		current.next = node;
+	}
+	public boolean isEmpty() {
+		return head == null;
+	}
+	public void delete(T data) {
+		if (isEmpty()) {
+			return ;
+		}
+		if (head.data.equals(data)) {
+			head = null;
+		}
+		Node<T> current = head;
+		while(current.next!=null) {
+			if (current.next.data.equals(data)) {
+				current.next = current.next.next;
+				return;
+			} else {
+				current = current.next;
+			}
 
+		}
+	}
+	public void display() {
+		Node<T> current = head;
+		while(current!=null) {
+			System.out.print(current.data);
+			current = current.next;
+		}
+		System.out.println();
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Node<T> {
-// 	T data;
-// 	Node<T> next;
-// 	public Node(T obj) {
-// 		data = obj;
-// 	}
-// }
-// public class LinkedList<T> {
-// 	private Node<T> head;
-// 	public void add (T data) {
-// 		if (head == null) {
-// 			head = new Node<>(data);
-// 			return;
-// 		}
-// 		Node current = head;
-// 		while (current.next != null) {
-// 			current = current.next;
-// 		}
-// 		current.next = new Node<>(data);
-// 	}
-// 	public void delete (T data) {
-// 		if (head == null) {
-// 			return ;
-// 		}
-// 		if (head.data.equals(data)) {
-// 			head = head.next;
-// 		}
-// 		Node current = head;
-// 		while (current.next != null) {
-// 			if (current.next.data.equals(data)) {
-// 				current.next = current.next.next;
-// 			} else {
-// 				current = current.next;
-// 			}
-// 		}
-// 	}
-// 	public void display() {
-// 		Node<T> current = head;
-// 		while (current != null) {
-// 			System.out.println(current.data + " ");
-// 			current = current.next;
-// 		}
-// 	}
-// }
-// class MainByLinkedList {
-// 	public static void main(String[] args) throws Exception{
-// 		LinkedList<Integer> list = new LinkedList();
-// 		list.add(1);
-// 		list.add(2);
-// 		list.add(3);
-// 		list.add(4);
-// 		list.add(5);
-// 		list.display();
-// 		list.delete(3);
-// 		list.display();
-// 	}
-// }
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(0);
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.display();
+		list.delete(3);
+		list.display();
+		list.delete(1);
+		list.display();
+	}
+}
--- src/Structure/Queue.java
+++ src/Structure/Queue.java
@@ -1,51 +1,51 @@
-package Structure;
-class Node<T> {
-	T data;
-	Node<T> next;
-	public Node(T data) {
-		this.data = data;
-	}
-}
-public class Queue<T> {
-	private Node<T> first;
-	private Node<T> last;
-	// 큐에 요소 추가 (enqueue)
-	public void enqueue(T data) {
-		Node<T> node = new Node<>(data);
-		if (last != null) {
-			last.next = node;
-		}
-		last = node;
-		if (first == null) {
-			first = last;
-		}
-	}
-	// 큐에서 요소 삭제하고 반환 (dequeue)
-	public T dequeue() {
-		if (isEmpty()) {
-			throw new IllegalArgumentException("비어있음");
-		}
-		T data = first.data;
-		first = first.next;
-		if (first == null) {
-			last = null;
-		}
-		return data;
-	}
-	// 큐의 맨 앞 요소 반환 (peek)
-	public T peek() {
-		if (isEmpty()) {
-			throw new IllegalArgumentException("비어있음");
-		}
-		return first.data;
-	}
-	// 큐가 비어있는지 여부 확인
-	private boolean isEmpty() {
-		return first == null;
-	}
-
-}
-class MainByQueue {
-	public static void main(String[] args) {
-	}
-}
+// package Structure;
+// class Node<T> {
+// 	T data;
+// 	Node<T> next;
+// 	public Node(T data) {
+// 		this.data = data;
+// 	}
+// }
+// public class Queue<T> {
+// 	private Node<T> first;
+// 	private Node<T> last;
+// 	// 큐에 요소 추가 (enqueue)
+// 	public void enqueue(T data) {
+// 		Node<T> node = new Node<>(data);
+// 		if (last != null) {
+// 			last.next = node;
+// 		}
+// 		last = node;
+// 		if (first == null) {
+// 			first = last;
+// 		}
+// 	}
+// 	// 큐에서 요소 삭제하고 반환 (dequeue)
+// 	public T dequeue() {
+// 		if (isEmpty()) {
+// 			throw new IllegalArgumentException("비어있음");
+// 		}
+// 		T data = first.data;
+// 		first = first.next;
+// 		if (first == null) {
+// 			last = null;
+// 		}
+// 		return data;
+// 	}
+// 	// 큐의 맨 앞 요소 반환 (peek)
+// 	public T peek() {
+// 		if (isEmpty()) {
+// 			throw new IllegalArgumentException("비어있음");
+// 		}
+// 		return first.data;
+// 	}
+// 	// 큐가 비어있는지 여부 확인
+// 	private boolean isEmpty() {
+// 		return first == null;
+// 	}
+//
+// }
+// class MainByQueue {
+// 	public static void main(String[] args) {
+// 	}
+// }
