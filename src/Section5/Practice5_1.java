4f46bff69350e571dcb222b09b433185ea04dd23
김민정 <mingmong0018@gmail.com>
2024-03-19 00:51:10 +0900

    feat: 올바른 괄호 문제 풀이 완료

---
+++ src/Section5/Practice5_1.java
@@ -0,0 +1,26 @@
+package Section5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Practice5_1 {
+	public String solution(String str) {
+		String answer = "YES";
+		Stack<Character> stack = new Stack<>();
+		for(char c: str.toCharArray()) {
+			if (c == '(') stack.push('(');
+			else {
+				if (stack.isEmpty()) answer = "NO";
+				stack.pop();
+			}
+		}
+		if (!stack.isEmpty()) answer = "NO";
+		return answer;
+	}
+	public static void main(String[] args) {
+		Practice5_1 p = new Practice5_1();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(p.solution(str));
+	}
+}
