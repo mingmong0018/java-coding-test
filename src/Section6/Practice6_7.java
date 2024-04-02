47bd6342363d5e170a7c2c09312dcdf2f825f331
김민정 <mingmong0018@gmail.com>
2024-04-02 17:26:29 +0900

    Comparable 구현하여 정렬 메서드 적용

--- src/Section6/Practice6_7.java
+++ src/Section6/Practice6_7.java
@@ -1,23 +1,37 @@
 package Section6;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice6_7 {
-	public static int[][] solution(int[][] arr) {
+	static class Point implements Comparable<Point>{
+		int x;
+		int y;
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			if (this.x == o.x) return this.y - o.y;
+			else return this.x - o.x;
+		}
+	}
+	public static Point[] solution(Point[] arr) {
+		Arrays.sort(arr);
 		return arr;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int[][] arr = new int[num][2];
+		Point[] arr = new Point[num];
 		for (int i = 0; i<num; i++) {
-			for (int j = 0; j<2; j++) {
-				arr[i][j] = sc.nextInt();
-			}
+			arr[i] = new Point(sc.nextInt(), sc.nextInt());
 		}
-		solution(arr);
-		for (int[] arr2 : arr) {
-			System.out.println(arr2[0] + " " + arr2[1]);
+		for (Point point: solution(arr)) {
+			System.out.println(point.x + " " + point.y);
 		}
 
 	}
